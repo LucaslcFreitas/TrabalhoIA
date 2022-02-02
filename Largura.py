34aa1a339d4c896dbd70a8284195f88f8fc98816
Samira <samira.barroso@estudante.ufjf.br>
2022-02-01 22:40:47 -0300

    Busca Largura

--- Largura.py
+++ Largura.py
@@ -1,2 +1,70 @@
+from Arvore import Arvore
+from NoArvore import NoArvore
+import random
+
 def executarLargura(barras):
-  return 0
+	# lista de abertos e fechados
+	abertos = []
+	fechados = []
+	banana = False
+	fracasso = False
+
+	# cria ávore vazia, com a raíz sendo o macaco
+	arvore = Arvore()
+	abertos.append(arvore.getRaiz())
+
+	while not banana and not fracasso:
+		if len(abertos) > 0:
+			N = abertos.pop(0)
+			
+			if N.getNoLista().getFinal():
+				banana = True
+				__imprimeCaminhoSolução(N, abertos, fechados)
+
+			else:
+				if N.getNoLista().getNome() == "M": #Faz o macaco escolher uma barra de maneira aleatória
+					auxiliarRNG = []
+					while len(auxiliarRNG) < 7:
+						rng = random.randint(0, 6)
+						if rng not in auxiliarRNG:
+							novoNoArv = NoArvore(barras[rng].getInicio(), N, False)
+							N.addFilho(novoNoArv)
+							abertos.append(novoNoArv)
+							auxiliarRNG.append(rng)
+				else:  # Movimentações do macaco
+					if not N.getOpAnterior():  # Se o macaco ainda nao andou pra cima
+						if N.getNoLista().getProximo() != None:
+							novoNoArv = NoArvore(
+								N.getNoLista().getProximo(), N, True)
+							N.addFilho(novoNoArv)
+							abertos.append(novoNoArv)
+					else:  # Troca de barra
+						if N.getNoLista().getTroca() != None:
+							novoNoArv = NoArvore(
+								N.getNoLista().getTroca(), N, False)
+							N.addFilho(novoNoArv)
+							abertos.append(novoNoArv)
+				fechados.append(N)
+		else:
+			fracasso = True
+		if fracasso:
+			print("Não foi possível encontrar a solução!")
+
+def __imprimeCaminhoSolução(no, listaAbertos, listaFechados):
+    print("Caminho encontrado!")
+    caminho = []
+    while no != None:
+        caminho.append(no.getNoLista().getNome())
+        no = no.getPai()
+    caminho = caminho[::-1]
+    print("Caminho solução: ", caminho)
+
+    abertos = []
+    for i in range(len(listaAbertos)):
+        abertos.append(listaAbertos[i].getNoLista().getNome())
+    print("Lista de abertos: ", abertos)
+
+    fechados = []
+    for i in range(len(listaFechados)):
+        fechados.append(listaFechados[i].getNoLista().getNome())
+    print("Lista de fechados: ", fechados, "\n") 
