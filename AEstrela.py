a94fd97f83096fcb6a37044b3239f74d0941c8d6
Rodrigo Torres <rodrigotorres@ice.ufjf.br>
2022-02-08 21:34:09 -0300

    desenhar arvore no arquivo

--- AEstrela.py
+++ AEstrela.py
@@ -1,5 +1,6 @@
 from Arvore import Arvore
 from NoArvore import NoArvore
+from CriarArvore import __criarArvore
 
 
 def executarAEstrela(barras):
@@ -29,6 +30,7 @@ def executarAEstrela(barras):
             # Caso o No atual seja o que tem a banana
             if noAtual.getNoLista().getFinal():
                 __imprimeCaminhoSolucao(noAtual, iteracao, abertos, fechados)
+                __criarArvore(arvore.getRaiz(), "AEstrela")
                 break
             else:
                 if noAtual.getNoLista().getNome() == "M":  # Regra R1
---
+++ CriarArvore.py
@@ -0,0 +1,10 @@
+from NoArvore import NoArvore
+
+
+def __criarArvore(raizArvore: NoArvore, nomeBusca):
+
+    f = open(f"arvores/{nomeBusca}.dot", "w")
+
+    f.write("")
+
+    f.close()
